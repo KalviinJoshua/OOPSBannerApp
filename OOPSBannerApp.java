a3483fde41fef44ee22a4f8174af74070f318d64
KalviinJoshua <kalviinjoshua747@gmail.com>
2026-03-02 08:28:03 +0530

    Remove documentation comments from OOPSBannerApp
    Removed class-level documentation comments from OOPSBannerApp.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,4 @@
-/**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application
- * that displays the Object-Oriented Programming System
- * acronym OOPS to the console.
- *
- * @author KalviinJoshua
- * @version 1.0
- */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
